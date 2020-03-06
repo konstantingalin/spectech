b8d47a11ac93f6bf461d933c4246b1870165e69b
имя_пользователя_github <__>
2020-03-07 01:52:29 +0600

    update two

--- js/common.js
+++ js/common.js
@@ -1,5 +1,32 @@
 $(function() {
-
-	// Пользовательские функции
-
+	
+	$('#my-menu').mmenu(
+	{
+		extensions: [ 'theme-black', 'fx-menu-slide', 'pagedim-black', "position-right"],
+		offCanvas: {
+			"position": "right"
+		}
+	});
+	
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin: 48,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:2,
+            dots:true,
+            loop:true
+        }
+    }
+});
+	
+	
+		
 });
